2f644c9939b098e0d38bc395da77b7cca930367b
Lisandro Leyria <l.leyria93@gmail.com>
2020-11-21 18:09:47 -0300

    Added getBuildingById resource

---
+++ controllers/getBuildingById.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const buildings = require('../data/buildings_mock_data');
+
+router.get('/:id', (req, res) => {
+    
+    const found = buildings.some(building => building.id === parseInt(req.params.id));
+    
+    if(found){
+        res.json(buildings.filter(building => building.id === parseInt(req.params.id))); 
+    } else {
+        res.status(404).json({msg: `No building with the ID of ${req.params.id}`});
+    }
+});
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const app = express();
 
 app.use('/api/buildings', require('./getBuildingsAll'));
+app.use('/api/buildings/', require('./getBuildingById'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log('Server started'));
