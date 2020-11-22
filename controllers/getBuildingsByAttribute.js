0c961e77ef0fb47250d682ce6a56d83dd92c4ac8
Lisandro Leyria <l.leyria93@gmail.com>
2020-11-22 20:38:03 -0300

    Fixed string and number value in request

--- controllers/getBuildingsByAttribute.js
+++ controllers/getBuildingsByAttribute.js
@@ -2,53 +2,19 @@ const express = require('express');
 const router = express.Router();
 const buildings = require('../data/buildings_mock_data');
 
-//Get Buildings by Address
-router.get('/byaddress/:address', (req, res) => {
+
+router.get('/:attribute/:value', (req, res) => {
     
-    const found = buildings.some(building => building.address === req.params.address);
+    const numberFound = buildings.some(building => building[req.params.attribute] === parseInt(req.params.value));
+    const stringFound = buildings.some(building => building[req.params.attribute] === req.params.value);
     
-    if(found){
-        res.json(buildings.filter(building => building.address === req.params.address)); 
+    if(numberFound){
+        res.json(buildings.filter(building => building[req.params.attribute] === parseInt(req.params.value)));
+    } else if(stringFound){
+        res.json(buildings.filter(building => building[req.params.attribute] === (req.params.value)));
     } else {
-        res.status(404).json({msg: `No building with ${req.params.id} as address`});
+        res.status(404).json({msg: `No building with ${req.params.value} as ${req.params.attribute}`});
     }
 });
-
-//Get Buildings by Boiler ID
-router.get('/byboilersid/:boilersId', (req, res) => {
-    
-    const found = buildings.some(building => building.boilersId === parseInt(req.params.boilersId));
-    
-    if(found){
-        res.json(buildings.filter(building => building.id === parseInt(req.params.boilersId))); 
-    } else {
-        res.status(404).json({msg: `No building with ${req.params.boilersId} as boilers id`});
-    }
-});
-
-//Get Buildings by Full Name
-router.get('/byfullname/:fullName', (req, res) => {
-    
-    const found = buildings.some(building => building.fullName === req.params.fullName);
-    
-    if(found){
-        res.json(buildings.filter(building => building.fullName === req.params.fullName)); 
-    } else {
-        res.status(404).json({msg: `No building with ${req.params.fullName} as full name`});
-    }
-});
-
-//Get Buildings by Phone
-router.get('/byphone/:phone', (req, res) => {
-    
-    const found = buildings.some(building => building.phone === req.params.phone);
-    
-    if(found){
-        res.json(buildings.filter(building => building.phone === req.params.phone)); 
-    } else {
-        res.status(404).json({msg: `No building with ${req.params.phone} as phone number`});
-    }
-});
-
 
 module.exports = router;
