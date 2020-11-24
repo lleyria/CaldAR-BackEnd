d29454f21438598f43a2b61f05821d8e815d07a4
Lisandro Leyria <l.leyria93@gmail.com>
2020-11-24 17:56:04 -0300

    Added fs.writeFileSync to deleteBuildingById function

--- controllers/deleteBuildingById.js
+++ controllers/deleteBuildingById.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const buildings = require('../data/buildings_mock_data');
+const path = require('path');
+const fs = require('fs');
 
 router.delete('/', (req, res) => {
     
     const found = buildings.some(building => building.id === parseInt(req.query.id));
     
     if(found){
-        res.json({msg: 'Member Deleted',
-        members: buildings.filter(building => building.id !== parseInt(req.query.id))}); 
+        const removedItem = buildings.filter(building => building.id !== parseInt(req.query.id))
+        fs.writeFileSync(path.join(__dirname,'../data/buildings_mock_data_test'), JSON.stringify(removedItem))
+        res.json(removedItem); 
     } else {
         res.status(404).json({msg: `No building with the ID of ${req.query.id}`});
     }
