e6dbbe4b01514873bc3edde85e97c65befee3692
Jose Luis Enriquez <josl.enriquez89@gmail.com>
2020-11-24 11:05:28 -0300

    Radium PS08 - WIP function getByAttribute complete

--- controllers/getBoilersByAttribute.js
+++ controllers/getBoilersByAttribute.js
@@ -1,20 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const boilers = require('../data/boilers.json');
-router.get('/api/boilers', (req, res) => {
+const boilers = require('../data/boilers');
+router.get('/', (req, res) => {
+    console.log(req.query);
     if(Object.keys(req.query).length !== 0){
-        const query = req.query;
-        const field = Object.getOwnPropertyNames(query);
-        const val = Object.values(query);
-        console.log(field[0]);
-        console.log(val[0]);
-        const found = boilers.some(boilers => boilers[field[0]] === val[0]);
-        console.log(found);
-        if(found){
-            res.json(boilers.filter(boilers => boilers[field[0]] === val[0]));
-        } else {
-            res.status(404).json({msg: `No boilers with ${field[0]} as ${val[0]}`});
-        }
+        return res.json(boilers.filter(boiler => boiler[req.query.attribute] == req.query.value));
+    } else {
+        res.status(404).json({msg: `No funciona`});
     }
 });
 module.exports = router;
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const getBoilersByAttribute = require('./controllers/getBoilersByAttribute');
 const deleteBoilerById = require('./controllers/deleteBoilerById');
 //app.use('/api/boilers', getAllBoilers);
 //app.use('/api/boilers', getBoilersById);
-app.use(getBoilersByAttribute);
+app.use('/api/boilers', getBoilersByAttribute);
 //app.use('/api/boilers', deleteBoilerById);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
