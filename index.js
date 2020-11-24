3787c9a14695222e05a8f3cd562c654a308dbe47
Carla Baleani <carlabaleani27@gmail.com>
2020-11-24 19:34:14 -0300

    Rename route

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.use('/appointment', require('./controllers/getAllAppointments'));
-app.use('/appointment', require('./controllers/deleteAppointmentsById'));
-app.use('/appointment', require('./controllers/getAppointmentById'));
-app.use('/appointment', require('./controllers/getAppointmentByAttribute'));
+app.use('/appointments', require('./controllers/getAllAppointments'));
+app.use('/appointments', require('./controllers/deleteAppointmentById'));
+app.use('/appointments', require('./controllers/getAppointmentById'));
+app.use('/appointments', require('./controllers/getAppointmentByAttribute'));
 
 const PORT = process.env.PORT || 5000;
 
