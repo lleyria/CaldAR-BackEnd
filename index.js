0e9a363b6293f59988932bfb4ca822f54ff6c1ca
Lisandro Leyria <l.leyria93@gmail.com>
2020-11-24 21:22:56 -0300

    Moved index.js out of controllers folder, deleted package.json

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.use('/api/buildings', require('./controllers/getBuildingsAll'));
+app.use('/api/buildings/', require('./controllers/getBuildingById'));
+app.use('/api/buildings/', require('./controllers/getBuildingsByAttribute'));
+app.use('/api/buildings/', require('./controllers/deleteBuildingById'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log('Server started'));
