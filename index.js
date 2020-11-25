607163c40c38831cf2f8c1aee3eddca083e59c6c
Lisandro Leyria <l.leyria93@gmail.com>
2020-11-24 21:27:00 -0300

    Linked index with resources, added server on localhost

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.use('/api/buildings', require('./controllers/getBuildingsAll'));
-app.use('/api/buildings/', require('./controllers/getBuildingById'));
-app.use('/api/buildings/', require('./controllers/getBuildingsByAttribute'));
-app.use('/api/buildings/', require('./controllers/deleteBuildingById'));
+app.use('/api/boilertypes', require('./controllers/getAllBoilerTypes'));
+app.use('/api/boilertypes', require('./controllers/getBoilerTypeById'));
+app.use('/api/boilertypes', require('./controllers/getBoilerTypesByAttribute'));
+app.use('/api/boilertypes', require('./controllers/deleteBoilerById'));
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log('Server started'));
