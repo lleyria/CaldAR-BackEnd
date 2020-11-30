321cdc0821d553962d5f6c8df547d1d6f4d0b506
Carla Baleani <carlabaleani27@gmail.com>
2020-11-30 11:06:05 -0300

    Import technicians' squema

--- app/models/index.js
+++ app/models/index.js
@@ -1,9 +1,10 @@
-const { DBRef } = require("mongodb");
+// const { DBRef } = require("mongodb");
 
 mongoose = require("mongoose");
 
 const db = {};
 db.mongoose = mongoose;
 db.url = "mongodb+srv://admin:admin123@clustercaldar.9cacc.mongodb.net/CaldAR?retryWrites=true&w=majority";
+db.technicians = require("./technicians.js")(mongoose);
 
 module.exports = db;
