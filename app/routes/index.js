ec2740e9fa54eb7ae5aae7d4ae02019ac05d9450
Carla Baleani <carlabaleani27@gmail.com>
2020-11-30 11:15:02 -0300

    set route for technicians entity

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,2 +1,8 @@
+const { route } = require("./technicians");
+const technicianRouter = require("./technicians");
+
 var router = require("express").Router();
+
+router.use("/technicians", technicianRouter);
+
 module.exports = router;
