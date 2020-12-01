4b9d1ca2441659bfefe9a0ab8d3b340539b83f0a
Carla Baleani <carlabaleani27@gmail.com>
2020-12-01 16:42:58 -0300

    Create appointments' model

--- app/models/index.js
+++ app/models/index.js
@@ -1,9 +1,8 @@
-const { DBRef } = require("mongodb");
-
 mongoose = require("mongoose");
 
 const db = {};
 db.mongoose = mongoose;
 db.url = "mongodb+srv://admin:admin123@clustercaldar.9cacc.mongodb.net/CaldAR?retryWrites=true&w=majority";
+db.appointments = require("./appointments.js")(mongoose);
 
 module.exports = db;
