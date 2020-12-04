31baf696548f4224a8ff3ee3e0cc709234b537ec
Carla Baleani <72134180+cbaleani@users.noreply.github.com>
2020-12-04 17:52:28 -0300

    Feature/cald 02 technicians (#9)
    * Import technicians' squema
    
    * set route for technicians entity
    
    * Create technicians data model
    
    * Add endpoints for CRUD operations on technicians
    
    * Add technician controller
    
    * several changes in the controller functions' logic
    
    * Add changes for use of query string instead of query params
    
    * Add API documentation for technicians collection
    
    * Rename each technician and name it as technicians. Fix documentation file.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,2 +1,6 @@
-var router = require("express").Router();
+const techniciansRouter = require("./technicians");
+const router = require("express").Router();
+
+router.use("/technicians", techniciansRouter);
+
 module.exports = router;
